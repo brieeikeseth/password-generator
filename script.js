6622d6d760fe85895621f1d9115bb895eca294de
Brie Eikeseth <brieeikeseth56@gmail.com>
2021-05-09 19:56:17 -0400

    all questions

--- script.js
+++ script.js
@@ -40,6 +40,83 @@ function generatePassword() {
     }else if (passwordLength < 8 || passwordLength > 128) {
         passwordLength = prompt ("You must choose between 8-128 characters")
         console.log("Password Length " + passwordLength);
-    }
+    } else { 
+        confirmLower = confirm("Will this contain lower case letters?");
+        console.log("Lower case " + confirmLower);
+        confirmUpper = confirm("Will this contain upper case letters?");
+        console.log("Upper case " + confirmUpper);
+        confirmNumber = confirm("Will this contain numbers?");
+        console.log("Number " + confirmNumber);
+        confirmSpecial = confirm("Will this contain special characters?");
+        console.log("Special Character " + confirmSpecial);
+    
+      };
+
+ // input left blank then 
+    if (!confirmLower && !confirmUpper && !confirmNumber && !confirmSpecial) {
+    userChoices = alert("You must choose a criteria to continue");
+ 
+    //  true option 1 (4 options)
+  } else if (confirmLower && confirmUpper && confirmNumber && confirmSpecial) {
+    userChoices = lower.concat(upper, numbers, special);
+    console.log(userChoices);
+  }
+
+  // true option 2 (3 options)
+  else if (confirmLower && confirmUpper && confirmSpecial) {
+    userChoices = lower.concat(upper, special);
+    console.log(userChoices);
+  }
+  else if (confirmLower && confirmNumber && confirmSpecial) {
+    userChoices = lower.concat(numbers, special);
+    console.log(userChoices);
+  }
+  
+  else if (confirmUpper && confirmNumber && confirmSpecial) {
+    userChoices = upper.concat(numbers, special);
+    console.log(userChoices);
+  }
+  // true option 3 (2)
+  else if (confirmLower && confirmUpper) {
+    userChoices = lower.concat(upper);
+    console.log(userChoices);
+  }
+  else if (confirmLower && confirmNumber) {
+    userChoices = lower.concat(numbers);
+    console.log(userChoices);
+  }
+  else if (confirmLower && confirmSpecial) {
+    userChoices = lower.concat(special);
+    console.log(userChoices);
+  }
+  else if (confirmUpper && confirmNumber) {
+    userChoices = upper.concat(numbers);
+    console.log(userChoices);
+  }
+  else if (confirmUpper && confirmSpecial) {
+    userChoices = upper.concat(special);
+    console.log(userChoices);
+  }
+  else if (confirmNumber && confirmSpecial) {
+    userChoices = numbers.concat(special);
+    console.log(userChoices);
+  }
+  // true option 4 (1)
+  else if (confirmLower) {
+    userChoices = lower;
+    console.log(userChoices);
+  }
+  else if (confirmUpper) {
+    userChoices = blankUpper.concat(upper);
+    console.log(userChoices);
+  }
+  else if (confirmNumber) {
+    userChoices = numbers;
+    console.log(userChoices);
+  }
+  else if (confirmSpecial) {
+    userChoices = special;
+    console.log(userChoices);
+  };
 
 }
